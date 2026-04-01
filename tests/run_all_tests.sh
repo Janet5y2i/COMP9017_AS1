59debc67030bb21519efa8ac5a24a735c4d708da
“Janetgit config --global user.email janet.chang.0110@gmail.com <janet.chang.0110@gmail.com>
2026-04-02 00:29:17 +1100

    add test case

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -47,7 +47,29 @@ fi
 
 
 echo "=== Third Test: check for the pixel data of the circle ==="
-xxd -g4 test_create_circle.dat > circle_answer.hex
+./test_create_circle && xxd -g4 test_create_circle.dat > circle_actual.hex
+
+# check if the expected file exist
+if [ ! -f "circle_expected.hex" ]; then
+    echo -e "${RED}[FAIL]${NC} No such file circle_expected.hex"
+    exit 1
+fi
+
+# check if the output exist
+if [ ! -f "circle_actual.hex" ]; then
+    echo -e "${RED}[FAIL]${NC} No output file circle_actual.hex"
+    exit 1
+fi
+
+# compare to the two files
+diff circle_actual.hex circle_expected.hex > /dev/null
+
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}[PASS]${NC} Pass animate_place_sprite test"
+else
+    echo -e "${RED}[FAIL]${NC} Pixel content mismatch"
+    diff -y --suppress-common-lines circle_actual.hex circle_expected.hex | head -n 10
+fi
 
 # 2. Using Valgrind to check for memory leaks and errors
 echo "Checking for memory leaks and errors..."
@@ -63,6 +85,121 @@ else
 fi
 
 # Clean up the executable
-rm -f ./tests/test_create_circle
-#rm -f ./tests/test_create_circle.dat
-#rm -f ./tests/circle_answer.hex
+rm -f test_create_circle
+rm -f test_create_circle.dat
+rm -f circle_actual.hex
+
+
+
+echo "=== Forth Test: check for the pixel data of the rectangle ==="
+
+# The forth case
+gcc -g -I.. test_create_rectangle_filled.c ../animate.o -o test_create_rectangle_filled
+
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Test - test_create_rectangle_filled failed to compile!${NC}"
+    exit 1
+fi
+
+
+./test_create_rectangle_filled && xxd -g4 test_create_rectangle_filled.dat > rectangle_filled_actual.hex
+
+# check if the expected file exist
+if [ ! -f "rectangle_filled_expected.hex" ]; then
+    echo -e "${RED}[FAIL]${NC} No such file rectangle_filled_expected.hex"
+    exit 1
+fi
+
+# check if the output exist
+if [ ! -f "rectangle_filled_actual.hex" ]; then
+    echo -e "${RED}[FAIL]${NC} No output file rectangle_filled_actual.hex"
+    exit 1
+fi
+
+# compare to the two files
+diff rectangle_filled_actual.hex rectangle_filled_expected.hex > /dev/null
+
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}[PASS]${NC} Pass animate_place_sprite test"
+else
+    echo -e "${RED}[FAIL]${NC} Pixel content mismatch"
+    diff -y --suppress-common-lines rectangle_filled_actual.hex rectangle_filled_expected.hex | head -n 10
+fi
+
+# 2. Using Valgrind to check for memory leaks and errors
+echo "Checking for memory leaks and errors..."
+valgrind --leak-check=full --error-exitcode=1 ./test_create_rectangle_filled > /dev/null 2>&1
+
+
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}[PASS]${NC} test_create_rectangle_filled (Function and memory are both normal)"
+else
+    echo -e "${RED}[FAIL]${NC} Memory leak or execution error detected!"
+    # If failed, run again to display detailed errors
+    valgrind --leak-check=full ./test_create_rectangle_filled
+fi
+
+
+
+# Clean up the executable
+rm -f test_create_rectangle_filled
+rm -f test_create_rectangle_filled.dat
+rm -f rectangle_filled_actual.hex
+
+
+
+
+echo "=== Fifth Test: check for the pixel data of the rectangle ==="
+
+# The fifth case
+gcc -g -I.. test_create_rectangle_empty.c ../animate.o -o test_create_rectangle_empty
+
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Test - test_create_rectangle_empty failed to compile!${NC}"
+    exit 1
+fi
+
+
+./test_create_rectangle_empty && xxd -g4 test_create_rectangle_empty.dat > rectangle_empty_actual.hex
+
+# check if the expected file exist
+if [ ! -f "rectangle_empty_expected.hex" ]; then
+    echo -e "${RED}[FAIL]${NC} No such file rectangle_empty_expected.hex"
+    exit 1
+fi
+
+# check if the output exist
+if [ ! -f "rectangle_empty_actual.hex" ]; then
+    echo -e "${RED}[FAIL]${NC} No output file rectangle_empty_actual.hex"
+    exit 1
+fi
+
+# compare to the two files
+diff rectangle_empty_actual.hex rectangle_empty_expected.hex > /dev/null
+
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}[PASS]${NC} Pass animate_place_sprite test"
+else
+    echo -e "${RED}[FAIL]${NC} Pixel content mismatch"
+    diff -y --suppress-common-lines rectangle_empty_actual.hex rectangle_empty_expected.hex | head -n 10
+fi
+
+# 2. Using Valgrind to check for memory leaks and errors
+echo "Checking for memory leaks and errors..."
+valgrind --leak-check=full --error-exitcode=1 ./test_create_rectangle_empty > /dev/null 2>&1
+
+
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}[PASS]${NC} test_create_rectangle_empty (Function and memory are both normal)"
+else
+    echo -e "${RED}[FAIL]${NC} Memory leak or execution error detected!"
+    # If failed, run again to display detailed errors
+    valgrind --leak-check=full ./test_create_rectangle_empty
+fi
+
+
+
+# Clean up the executable
+rm -f test_create_rectangle_empty
+rm -f test_create_rectangle_empty.dat
+rm -f rectangle_empty_actual.hex
---
+++ tests/test_create_rectangle_filled.c
@@ -0,0 +1,73 @@
+/**
+ * A simple test file to help you get started
+ */
+
+#include "animate.h"
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define OUTPUT_FILE "test_create_rectangle_filled.dat"
+
+struct sprite {
+    // TODO
+    size_t width;
+    size_t height;
+    color_t color;
+    color_t *pixels;
+    bool filled;
+    int cnt;
+
+};
+
+int main(int argc, char** argv) {
+    size_t height = 2;
+    size_t width = 2;
+    color_t c = animate_color_argb(255, 255, 255, 255);
+    struct sprite* rectangle = animate_create_rectangle(width, height, c, 1);
+
+    // 1. create a circle sprite and check if it's created successfully
+    if (rectangle == NULL) {
+        fprintf(stderr, "Error: Failed to create rectangle sprite (NULL)\n");
+        return 1;
+    }
+
+
+    // 2. check the sprite_placement function and animate_create_circle
+    // if the rectangle is right and can be place on the canvas
+    color_t canvas_color = animate_color_argb(0, 0, 0, 0);
+    struct canvas* canvas = animate_create_canvas(8, 8, canvas_color);
+    if (canvas == NULL) {
+        animate_destroy_sprite(rectangle);
+        return 1;
+    }
+
+    struct sprite_placement* placement = animate_place_sprite(canvas, rectangle, 0,0);
+
+    if (placement == NULL){
+        animate_destroy_canvas(canvas);
+        animate_destroy_sprite(rectangle);
+        fprintf(stderr, "Error: Failed to place the cirectanglercle sprite on the canvas (NULL)\n");
+        return 1;
+    }
+
+    
+    size_t frame_size_bytes = animate_frame_size_bytes(canvas);
+    void* data = malloc(frame_size_bytes);
+    animate_generate_frame(canvas, 0, 1, data);
+
+    FILE* fp = fopen(OUTPUT_FILE, "wb");
+    fwrite(data, 1, frame_size_bytes, fp);
+    fclose(fp);
+
+    free(data);
+    
+    animate_destroy_canvas(canvas);
+    animate_destroy_sprite(rectangle);
+    return 0;
+
+}
